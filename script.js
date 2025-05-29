1274e4773ce2e0337de575314350fb7b2586e1d7
Jur-byte <zjw218@yahoo.com>
2025-05-29 03:41:02 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function changeText() {
-    let p = document.querySelector('p');
+    let p = document.querySelector('#toggleText');
     p.innerText = p.innerText === 'You clicked the button!' ? 'This is a test site to learn web development.' : 'You clicked the button!';
+}
+function changeImage() {
+    let img = document.querySelector('img');
+    img.src = img.src === 'https://picsum.photos/300/200' ? 'https://picsum.photos/300/201' : 'https://picsum.photos/300/200';
 }

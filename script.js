1755c581d147817ea72332c3eb8756df83e7a4bc
Jur-byte <zjw218@yahoo.com>
2025-05-29 03:48:43 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,7 @@ function changeImage() {
     let img = document.querySelector('img');
     img.src = img.src === 'https://picsum.photos/300/200' ? 'https://picsum.photos/300/201' : 'https://picsum.photos/300/200';
 }
+function toggleButtonColor() {
+    let button = document.querySelector('button[onclick="toggleButtonColor()"]');
+    button.style.backgroundColor = button.style.backgroundColor === 'darkblue' ? 'navy' : 'darkblue';
+}

453d1d7133858d175cffa0f41b86187bfd28e6b4
Jur-byte <zjw218@yahoo.com>
2025-05-29 04:32:51 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function changeText() {
     let p = document.querySelector('#toggleText');
-    p.innerText = p.innerText === 'You clicked the button!' ? 'This is a test site to learn web development.' : 'You clicked the button!';
+    if (p) p.innerText = p.innerText === 'You clicked the button!' ? 'This is a test site to learn web development.' : 'You clicked the button!';
 }
 function changeImage() {
     let img = document.querySelector('img');
-    img.src = img.src.includes('200') ? 'https://picsum.photos/300/201' : 'https://picsum.photos/300/200';
+    if (img) img.src = img.src.includes('200') ? 'https://picsum.photos/300/201' : 'https://picsum.photos/300/200';
 }
 function toggleButtonColor() {
     let button = document.querySelector('button[onclick="toggleButtonColor()"]');
-    button.style.backgroundColor = button.style.backgroundColor === 'darkblue' ? 'darkgreen' : 'darkblue';
+    if (button) button.style.backgroundColor = button.style.backgroundColor === 'darkblue' ? 'darkgreen' : 'darkblue';
 }
 function highlightNav() {
     let links = document.querySelectorAll('nav a');
